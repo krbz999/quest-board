d1cb6749e90bbbed55034605d03d7f05cf39d2b8
Zhell <kennethzhell@gmail.com>
2025-07-06 17:11:49 +0200

    Remove redundant state handling.

--- scripts/applications/api/abstract-page-sheet.mjs
+++ scripts/applications/api/abstract-page-sheet.mjs
@@ -54,7 +54,7 @@ export default class AbstractPageSheet extends foundry.applications.sheets.journ
   async _onRender(context, options) {
     await super._onRender(context, options);
 
-    new foundry.applications.ux.DragDrop({
+    new foundry.applications.ux.DragDrop.implementation({
       dropSelector: ".drop-area",
       callbacks: {
         drop: this._onDrop.bind(this),
@@ -72,32 +72,4 @@ export default class AbstractPageSheet extends foundry.applications.sheets.journ
    * @param {Event} event   The initiating drop event.
    */
   _onDrop(event) {}
-
-  /* -------------------------------------------------- */
-
-  /** @inheritdoc */
-  _preSyncPartState(partId, newElement, priorElement, state) {
-    super._preSyncPartState(partId, newElement, priorElement, state);
-    if (state.focus) return;
-    const focused = priorElement.querySelector(":focus");
-    if (!focused) return;
-    if (focused.parentElement.tagName === "STRING-TAGS") {
-      state.focus = [focused.parentElement.name, focused.tagName, partId].join(":");
-      state.focusSpc = true;
-    }
-  }
-
-  /* -------------------------------------------------- */
-
-  /** @inheritdoc */
-  _syncPartState(partId, newElement, priorElement, state) {
-    if (state.focusSpc) {
-      const [focus, tagName, _partId] = state.focus.split(":");
-      if (_partId === partId) {
-        newElement.querySelector(`string-tags[name="${focus}"] > ${tagName}`).focus();
-        delete state.focus;
-      }
-    }
-    super._syncPartState(partId, newElement, priorElement, state);
-  }
 }
